b31b56077d681534464a1f810e7840fd623d3cc3
Okoroama Chisom Charles <okoroamachisom@gmail.com>
2025-05-26 07:08:31 -0700

    First bug-free python exercise

--- basic_operation.py
+++ basic_operation.py
@@ -1,5 +1,9 @@
 number1 = 10
 number2 = 5
-Addition of 10 and 5 is 15
-Subtraction of 10 and 5 is 5
-Multiplication of 10 and 5 is 50
+Addition = number1 + number2
+print("Addition of", number1, "and", number2, "is", Addition)
+Addition = number1 + number2
+Difference = number1 - number2
+Product = number1 * number2
+print("Subtraction of", number1, "and", number2, "is", Difference)
+print("Multiplication of", number1, "and", number2, "is", Product)
